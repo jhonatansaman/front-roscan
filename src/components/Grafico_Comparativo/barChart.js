783071a0e2775123ecaa8e2d33aefda2de190d29
Jhonatan Saman <jhonatansaman@Jhonatans-MacBook-Pro.local>
2019-10-13 16:35:33 -0300

    alter

--- src/components/Grafico_Comparativo/barChart.js
+++ src/components/Grafico_Comparativo/barChart.js
@@ -200,6 +200,7 @@ export default class barChart extends Component {
 
     }
 
+    //esses são meus tempos pré
     async temposPre() {
         const response = await api.post('/recuperacao/buscarTemposPre', {
             idrecuperacao: this.props.id_recuperacao
